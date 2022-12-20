0792603601a3762cfea0ed6446db0472ee0b82e5
Varshil <varshil.shah@ecosmob.com>
2022-12-20 16:52:18 +0530

    upgrade env file

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = app.listen(port, () => {
     const getData = () => {
       axios
         .get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false",
+          process.env.api,
           {
             headers: { "Accept-Encoding": "gzip,deflate,compress" },
           }
